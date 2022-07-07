1253da0d53ee665b203e0a9429f16f54a90e7d30
TomiRenaldi <tomirenaldi2@gmail.com>
2022-07-07 21:35:43 +0700

    :sparkles: Add UnoCSS

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       imports: [
         'vue'
       ]
-    })
+    }),
+    UnoCSS(),
   ],
 })
